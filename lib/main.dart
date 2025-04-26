995dc8466f5176fcbeb8b351479e4fa0dab197f6
Cathrine <cathrine@example.com>
2025-04-26 04:24:51 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class notes_app extends StatelessWidget {
+  const notes_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
